21851246a6a00a5e30878896611ad42cd8e63175
F Davis Quarles <fdq0001@auburn.edu>
2015-10-07 20:43:17 -0500

    set countdown time

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	/* ---- Countdown timer ---- */
 
 	$('#counter').countdown({
-		timestamp : (new Date()).getTime() + 51*24*60*60*1000
+		timestamp : (new Date(2016,8,3))
 	});
 
 
